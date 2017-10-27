bde040af788020f01e044730e6a300f4acd550ba
ddorn <diego.dorn@free.fr>
2017-10-27 19:14:02 +1100

    updated example

--- superprompt/example.py
+++ superprompt/example.py
@@ -4,11 +4,13 @@ def complete(text):
     return [text + str(i) for i in range(len(text))]
 
 def main():
-    print(core.prompt_autocomplete('Custom', complete))
-    print(core.prompt_file('File'))
-    print(core.prompt_file('Directory', None, True, True))
-    print(core.prompt_choice('Favourite color', 'red green blue purple orange pink purple black brown'.split()))
-    print(core.prompt_choice('Favourite color', 'red green blue orange pink purple black brown purple'.split(), 'orange'))
-
+    colors = 'red green blue magenta white cyan yellow black'.split()
+    core.prompt_autocomplete('Custom', complete, )
+    color = core.prompt_choice('Favourite color', colors)
+    core.prompt_file('File', color=color)
+    core.prompt_file('Directory', None, True, True, color)
+    core.prompt_choice('Favourite color', colors, 'blue', color=color)
+    core.prompt_choice('Worst color', colors, show_default=False, default='red')
+    
 if __name__ == '__main__':
     main()
